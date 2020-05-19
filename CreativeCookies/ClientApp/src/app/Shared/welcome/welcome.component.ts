d13e4502c92d2d6d603f598a2662088e22caf53d
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-05-19 16:40:21 +0200

    Implementing raw video upload (in welcomeComponent for now)

--- CreativeCookies/ClientApp/src/app/Shared/welcome/welcome.component.ts
+++ CreativeCookies/ClientApp/src/app/Shared/welcome/welcome.component.ts
@@ -66,5 +66,4 @@ export class WelcomeComponent implements OnInit {
     })
     );
   }
-
 }
--- CreativeCookies/Controllers/VideosController.cs
+++ CreativeCookies/Controllers/VideosController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
@@ -22,9 +23,38 @@ namespace CreativeCookies.Controllers
         }
         [HttpPost]
         [AllowAnonymous]
-        public IActionResult UploadVideo(IFormFile file)
+        public async Task<ActionResult> UploadVideo(IFormFile file)
         {
-            return Ok($"{file.FileName} size: {file.Length} bytes");
+            var availableFormats = new List<string>
+            {
+                "3gp","asf","avi","dv","dvd","flv","m2ts",
+                "mkv","mov","mp4","mpg","ogg","smv", "svcd",
+                "ts", "wmv", "vcd"
+            };
+            if (!availableFormats.Contains(file.FileName.Split(".")[1].ToLower()))
+            {
+                return BadRequest("Only formats listed at https://pl.wikipedia.org/wiki/Formaty_plik%C3%B3w_wideo are supported.");
+            }
+            var path = @"A://CreativeCookiesStorage";
+            // now implement the procedure, to store uploaded video on a hard drive.
+            //return Ok($"{file.FileName} size: {file.Length} bytes");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            // ID of a file
+            var uniqueFileName = Guid.NewGuid().ToString();
+            var fileName = Path.GetFileName(file.FileName.Split(".")[0].ToLower() + "." + file.FileName.Split(".")[1].ToLower());
+            path = path + @"/";
+            string fullPath = path + fileName;
+
+
+
+            var fileStream = new FileStream(fullPath, FileMode.Create);
+            await file.CopyToAsync(fileStream);
+
+            return Ok($"Check the folder: {path}");
         }
     }
 }
