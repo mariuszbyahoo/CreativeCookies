2caefbbb3141eca31232b000e17fff78c9fca9e1
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-05-20 14:23:19 +0200

    Binding videoUrl property to displayed video.

--- CreativeCookies/ClientApp/src/app/Posts/post-details.component.ts
+++ CreativeCookies/ClientApp/src/app/Posts/post-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import { IPost } from './post';
 import { PostService } from './post.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './post-details.component.html',
@@ -11,13 +12,16 @@ export class PostDetailsComponent implements OnInit {
   pageTitle:  string = "Post Details";
   post: IPost;
   errMsg: string;
+  sanitizedVideoUrl: SafeResourceUrl;
 
-  constructor(private route: ActivatedRoute, private postSrv: PostService, private router: Router) { }
+  constructor(private route: ActivatedRoute, private postSrv: PostService, private router: Router,
+              private domSanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.postSrv.getPost(this.route.snapshot.paramMap.get('id')).subscribe({
       next: post => {
         this.post = post
+        this.sanitizedVideoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.post.videoUrl);
       },
       error: err => {
         this.errMsg = err;
--- CreativeCookies/ClientApp/src/app/Shared/welcome/welcome.component.ts
+++ CreativeCookies/ClientApp/src/app/Shared/welcome/welcome.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpEventType } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { isNullOrUndefined } from 'util';
+//import { HttpClient, HttpEventType } from '@angular/common/http';
+//import { map } from 'rxjs/operators';
+//import { FormBuilder, FormGroup } from '@angular/forms';
+//import { isNullOrUndefined } from 'util';
 
  
 @Component({
@@ -11,62 +11,63 @@ import { isNullOrUndefined } from 'util';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-  form: FormGroup;
+  //form: FormGroup;
   error: string;
   uploadResponse = { status: '', message: '', filePath: '' };
-  constructor(private _http: HttpClient, private formBuilder: FormBuilder) { }
+  constructor(/*private _http: HttpClient, private formBuilder: FormBuilder*/) { }
 
   ngOnInit() {
-    this.form = this.formBuilder.group({
-      video: ['']
-    });
-  }
-
-  onFileChange(event) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.form.get('video').setValue(file);
-    }
-  }
-
-  onSubmit() {
-    const formData = new FormData();
-    formData.append('file', this.form.get('video').value);
-
-    this.uploadVideoToServer(formData).subscribe(
-      (res) => {
-        this.uploadResponse = res;
-        console.log(res);
-      },
-      (err) => {
-        // Upload completing is being treated as an error, dunno why
-        if (isNullOrUndefined(err.error.text)) { this.error = err.error; }
-        else { this.error = err.error.text }
-        console.log(err);
-      }
-    );
-  }
-
-  public uploadVideoToServer(data) {
-    let uploadURL = `https://localhost:44370/api/videos`;
-
-    return this._http.post<any>(uploadURL, data, {
-      reportProgress: true,
-      observe: 'events'
-    }).pipe(map((event) => {
-
-      switch (event.type) {
-
-        case HttpEventType.UploadProgress:
-          const progress = Math.round(100 * event.loaded / event.total);
-          return { status: 'progress', message: progress };
-
-        case HttpEventType.Response:
-          return event.body;
-        default:
-          return `Unhandled event: ${event.type}`;
-      }
-    })
-    );
+    //this.form = this.formBuilder.group({
+    //  video: ['']
+    //});
   }
 }
+
+//  onFileChange(event) {
+//    if (event.target.files.length > 0) {
+//      const file = event.target.files[0];
+//      this.form.get('video').setValue(file);
+//    }
+//  }
+
+//  onSubmit() {
+//    const formData = new FormData();
+//    formData.append('file', this.form.get('video').value);
+
+//    this.uploadVideoToServer(formData).subscribe(
+//      (res) => {
+//        this.uploadResponse = res;
+//        console.log(res);
+//      },
+//      (err) => {
+//        // Upload completing is being treated as an error, dunno why
+//        if (isNullOrUndefined(err.error.text)) { this.error = err.error; }
+//        else { this.error = err.error.text }
+//        console.log(err);
+//      }
+//    );
+//  }
+
+//  public uploadVideoToServer(data) {
+//    let uploadURL = `https://localhost:44370/api/videos`;
+
+//    return this._http.post<any>(uploadURL, data, {
+//      reportProgress: true,
+//      observe: 'events'
+//    }).pipe(map((event) => {
+
+//      switch (event.type) {
+
+//        case HttpEventType.UploadProgress:
+//          const progress = Math.round(100 * event.loaded / event.total);
+//          return { status: 'progress', message: progress };
+
+//        case HttpEventType.Response:
+//          return event.body;
+//        default:
+//          return `Unhandled event: ${event.type}`;
+//      }
+//    })
+//    );
+//  }
+//}
