b87d7d1a25247de8664ceb2f8518444566c6739d
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-05-25 18:09:34 +0200

    Deleting Videos classes & associated plus classes used by Azure Cosmos DB

--- CreativeCookies.Data/PostsContext.cs
+++ CreativeCookies.Data/PostsContext.cs
@@ -13,7 +13,7 @@ namespace CreativeCookies.Data
     /// <summary>
     /// Performs CreateReadUpdateDelete operations on the associated DataBase
     /// </summary>
-    public class PostsContext : DbContext
+    public class PostsContext: DbContext
     {
         public DbSet<Post> Posts { get; set; }
 
@@ -107,80 +107,5 @@ namespace CreativeCookies.Data
                 .FirstOrDefault());
             SaveChanges();
         }
-
-        //private Container _container;
-
-        //public PostsContext(CosmosClient cosmosClient, string dbId, string containerId)
-        //{
-        //    _container = cosmosClient.GetContainer(dbId, containerId);
-        //}
-
-        ///// <summary>
-        ///// Create & Save Changes
-        ///// </summary>
-        ///// <param name="newPost"></param>
-        //public async Task Create(Post newPost)
-        //{  
-        //    await _container.CreateItemAsync<Post>(newPost, new PartitionKey(newPost.Id.ToString()));
-        //}
-
-        ///// <summary>
-        ///// Read
-        ///// </summary>
-        ///// <param name="ID"></param>
-        ///// <returns></returns>
-        //public async Task<Post> Read(Guid ID)
-        //{
-        //    var id = ID.ToString();
-        //    try
-        //    {
-        //        ItemResponse<Post> response = await this._container.ReadItemAsync<Post>(id, new PartitionKey(id));
-        //        return response.Resource;
-        //    }
-        //    catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
-        //    {
-        //        return null;
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Read all posts.
-        ///// We may see a quite different approach to talking with a DB than in traditional SQL
-        ///// </summary>
-        ///// <returns></returns>
-        //public async Task<List<Post>> Read()
-        //{
-        //    var query = this._container.GetItemQueryIterator<Post>("Select * from posts");
-        //    List<Post> results = new List<Post>();
-        //    while (query.HasMoreResults)
-        //    {
-        //        var response = await query.ReadNextAsync();
-
-        //        results.AddRange(response.ToList());
-        //    }
-
-        //    return results;
-        //}
-
-        ///// <summary>
-        ///// Delete & Save Changes
-        ///// </summary>
-        ///// <param name="ID"></param>
-        ///// <returns></returns>
-        //public async Task Delete(Guid ID)
-        //{
-        //    var id = ID.ToString();
-        //    await this._container.DeleteItemAsync<Post>(id, new PartitionKey(id));
-        //}
-
-        ///// <summary>
-        ///// Update & Save Changes
-        ///// </summary>
-        ///// <param name="newPost"></param>
-        //public async Task Update(Post newPost)
-        //{
-        //    var id = newPost.Id.ToString();
-        //    await this._container.ReplaceItemAsync<Post>(newPost, id);
-        //}
     }
 }
