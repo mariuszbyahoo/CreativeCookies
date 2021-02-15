c7ff801ebec368e7bc70c07c321f7492df21ac67
mariusz.budzisz <mariusz.budzisz@morlab.onmicrosoft.com>
2021-02-15 20:43:52 +0100

    clearing commentaries

--- CreativeCookies.IdentityServer/Quickstart/Account/AccountController.cs
+++ CreativeCookies.IdentityServer/Quickstart/Account/AccountController.cs
@@ -32,7 +32,6 @@ namespace Creativecookies.identityserver
 {
     /// <summary>
     /// This sample controller implements a typical login/logout/provision workflow for local and external accounts.
-    /// The login service encapsulates the interactions with the user data store. This data store is in-memory only and cannot be used for production!
     /// The interaction service provides a way for the UI to communicate with identityserver for validation and context retrieval
     /// </summary>
     [SecurityHeaders]
