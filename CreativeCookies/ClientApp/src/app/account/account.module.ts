85269c643437e1fc58685777cd4704f9b80ae721
mariusz.budzisz <mariusz.budzisz@morlab.onmicrosoft.com>
2021-03-01 15:07:10 +0100

    deleting login-dialog components & associated

--- CreativeCookies/ClientApp/src/app/account/account.module.ts
+++ CreativeCookies/ClientApp/src/app/account/account.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RegisterSuccessComponent } from './register-success/register-success.component';
 import { SharedModule } from '../shared/shared.module';
 import { RegisterDialogComponent } from './register-dialog/register-dialog.component';
-import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { RegisterDialogContentComponent } from './register-dialog/register-dialog-content/register-dialog-content.component';
-import { LoginDialogContentComponent } from './login-dialog/login-dialog-content/login-dialog-content.component';
 import { TermsComponent } from './terms/terms.component';
 
 
 
 @NgModule({
   declarations: [
-    RegisterComponent,
-    RegisterSuccessComponent,
     RegisterDialogComponent,
     RegisterDialogContentComponent,
-    LoginDialogComponent,
-    LoginDialogContentComponent,
     TermsComponent
   ],
   imports: [
@@ -29,16 +21,12 @@ import { TermsComponent } from './terms/terms.component';
     SharedModule,
     ReactiveFormsModule,
     RouterModule.forChild([
-      { path: 'register', component: RegisterComponent, pathMatch: 'full' },
-      { path: 'register/success', component: RegisterSuccessComponent, pathMatch: 'full' },
       { path: 'terms', component: TermsComponent, pathMatch:'full' }
     ])
   ],
   exports: [
     RegisterDialogComponent,
     RegisterDialogContentComponent,
-    LoginDialogComponent,
-    LoginDialogContentComponent
   ]
 })
 export class AccountModule { }
--- CreativeCookies/ClientApp/src/app/account/register/IAccountForm.ts
+++
@@ -1,6 +0,0 @@
-export interface IAccountForm {
-    email: string;
-    password: string;
-    confirmPassword: string;
-    username: string;
-}
