5e397dd9aa975e25fa06169f26462d0473ff7efb
mariusz.budzisz <mariusz.budzisz@morlab.onmicrosoft.com>
2021-05-03 14:05:18 +0200

    adding material-ui

--- creativecookies-app/src/App.js
+++ creativecookies-app/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+// uses material-ui.com
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
+      <Button variant='contained' color='primary'>
+        Hello World
+      </Button>
     </div>
   );
 }
