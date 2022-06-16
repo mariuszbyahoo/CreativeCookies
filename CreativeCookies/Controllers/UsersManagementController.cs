1739169760bde8f7448c1261aae0d56bd561a9fe
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2022-06-16 12:05:53 +0200

    Adding UsersManagementController's actions description

--- CreativeCookies/Controllers/UsersManagementController.cs
+++ CreativeCookies/Controllers/UsersManagementController.cs
@@ -8,15 +8,26 @@ namespace CreativeCookies.Controllers
     [Authorize(Roles = "admin")]
     public class UsersManagementController : ControllerBase
     {
+        /// <summary>
+        /// Returns an IEnumerable of all registered users in app
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult GetAllUsers()
         {
             // HACK: TODO
             return Ok();
         }
 
+        /// <summary>
+        /// Sends an e-mail to selected users.
+        /// </summary>
+        /// <param name="usersIds">Adressees</param>
+        /// <param name="emailSubject">Subject of an e-mail</param>
+        /// <param name="emailContent">Content of an e-mail</param>
+        /// <returns></returns>
         [HttpPost("sendMail")]
-        public IActionResult SendMailToUsers(IEnumerable<int> usersIds, string emailSubject, string emailContext)
+        public IActionResult SendMailToUsers(IEnumerable<int> usersIds, string emailSubject, string emailContent)
         {
             // HACK: TODO
 
@@ -28,6 +39,11 @@ namespace CreativeCookies.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Toggles an access to the app for a specific user
+        /// </summary>
+        /// <param name="userId">ID of an user to toggle an access for</param>
+        /// <returns></returns>
         [HttpPut("toggleAccess")]
         public IActionResult ToggleAccessForUser(int userId)
         {
@@ -36,6 +52,11 @@ namespace CreativeCookies.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Toggles an access to the app geographically, i.e. for all users from a specific country
+        /// </summary>
+        /// <param name="countryCode">Code of an country to toggle an access for</param>
+        /// <returns></returns>
         [HttpPut("toggleAccessGeographically")]
         public IActionResult ToggleAccessGeographically(string countryCode)
         {
