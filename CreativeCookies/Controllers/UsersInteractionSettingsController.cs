3f08103854422e44dba4c4758bd6b1da6a0919e0
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2022-06-16 12:00:32 +0200

    adding UsersInteractionSettingsController

---
+++ CreativeCookies/Controllers/UsersInteractionSettingsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreativeCookies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin")]
+    public class UsersInteractionSettingsController : ControllerBase
+    {
+        /// <summary>
+        /// Differentiates, does commenting videos should be possible (and visible) for paid users
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult ToggleComments()
+        {
+            // HACK: TODO
+            return Ok();
+        }
+
+        /// <summary>
+        /// Differentiates, does rating a videos should be possible for paid users
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult ToggleVideoRating()
+        {
+            // HACK: TODO
+            return Ok();
+        }
+    }
+}
