b38af2148e20a29a0dbaa5066c1fa23e30ad43b2
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2022-06-16 11:41:10 +0200

    adding VideoTradeController

---
+++ CreativeCookies/Controllers/VideoTradeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreativeCookies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin")]
+    public class VideoTradeController : ControllerBase
+    {
+        [HttpPut]
+        public IActionResult TogglePaidSubscription()
+        {
+            // HACK: TODO 
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult ToggleTradeOfIndividualVideos()
+        {
+            // HACK: TODO
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult ToggleDonations()
+        {
+            // HACK: TODO
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult TogglePremiumSubscribentAccount()
+        {
+            // HACK: TODO
+            return Ok();
+        }
+    }
+}
